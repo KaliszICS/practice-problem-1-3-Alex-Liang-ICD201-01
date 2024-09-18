c72965651e6c630e350aced8e2766c49519d1ba4
Alex-Liang-ICD201-01 <alex.liang3@student.tdsb.on.ca>
2024-09-18 14:46:52 +0000

    COmmit

--- PP1_3.py
+++ PP1_3.py
@@ -1,25 +1,31 @@
 
+'''
+Lesson 3 - Using Strings
+Author: Alex Liang
+Date Issued: September 17th, 2024
+Last Modified: September 18th, 2024
+'''
 
 def q1():
   #Write Assignment code here
   #Remember your code must be indented once
 
-  print("\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n")
+  print(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n")
 
-  print("\n* * * * *\n*       *\n*       *\n*       *\n* * * * *")
+  print("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n")
 
-  print("\n* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
+  print("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *")
 
 def q2():
   #Write Assignment code here
 
   print("Hello", end = " ")
-  print("World", end = "")
+  print("World")
 
 def q3():
   #Write Assignment code here
 
-  print('\n"Man"\n"Board"\n"Man Overboard"')
+  print('"Man"\n"Board"\n"Man Overboard"')
   
 
 #Do not alter anything after this line
